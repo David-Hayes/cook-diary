73fea8786f58e6a6923e32eae50fdd14a1dda690
David Hayes <davidhayes88@gmail.com>
2020-04-22 20:30:44 +0100

    Header tidy and added add icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,30 @@
 import React from 'react'
 
 import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginBottom: theme.spacing(3),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
 
 export const Header = () => {
+  const classes = useStyles()
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6">Cook Diary</Typography>
-      </Toolbar>
-    </AppBar>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>Cook Diary</Typography>
+          <AddIcon />
+        </Toolbar>
+      </AppBar>
+    </div>
   )
 }
