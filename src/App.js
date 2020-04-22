c3fb5b4e3f96997f892efb2d53f596648c39fa61
David Hayes <davidhayes88@gmail.com>
2020-04-22 20:24:54 +0100

    Dark theme

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import { Header } from './components/Header'
 import { CookList } from './components/CooksList'
 
 import { CssBaseline } from '@material-ui/core'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+})
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Header />
       <GlobalProvider>
         <CookList />
       </GlobalProvider>
-    </>
+    </ThemeProvider>
   )
 }
 
