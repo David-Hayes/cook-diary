2269e78206d4220532a901b867ba1b512e20624a
David Hayes <davidhayes88@gmail.com>
2020-04-27 20:05:33 +0100

    Reset

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import React from 'react'
-import { GlobalProvider } from './context/State'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
-import { Header } from './components/Header'
-import { CookList } from './components/CooksList'
-import { Add } from './components/Add'
-
-import { CssBaseline } from '@material-ui/core'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
-
-const darkTheme = createMuiTheme({
-  palette: {
-    type: 'dark',
-  },
-})
+const netlifyIdentity = require('netlify-identity-widget')
 
 function App() {
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Router>
-        <Header />
-        <GlobalProvider>
-          <Switch>
-            <Route path="/add">
-              <Add />
-            </Route>
-            <Route path="/">
-              <CookList />
-            </Route>
-          </Switch>
-        </GlobalProvider>
-      </Router>
-    </ThemeProvider>
-  )
+
+  netlifyIdentity.open();
+
+  return <></>
 }
 
 export default App
