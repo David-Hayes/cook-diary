7b65011beb6def22edff39e440ee51a26f2a50f7
David Hayes <davidhayes88@gmail.com>
2020-04-27 23:45:57 +0100

    State and basic toolbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react'
-import { GlobalProvider } from './context/State'
 import netlifyIdentity from 'netlify-identity-widget'
 
 import { Header } from './components/Header'
 
 import { CssBaseline } from '@material-ui/core'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+})
 
 netlifyIdentity.init()
 
 function App() {
   return (
-    <GlobalProvider>
+    <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Header />
-    </GlobalProvider>
+    </ThemeProvider>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,50 @@
-import React, { useContext, useEffect } from 'react'
-import { GlobalContext } from '../context/State'
+import React, { useState, useEffect } from 'react'
+import netlifyIdentity from 'netlify-identity-widget'
+
+import { AppBar, Toolbar, Typography, Link } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginBottom: theme.spacing(3),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
 
 export const Header = () => {
-  const { loggedIn, setLoggedIn } = useContext(GlobalContext)
+  const classes = useStyles()
+  const [loggedIn, setLoggedIn] = useState(!!netlifyIdentity.currentUser())
 
-  useEffect(setLoggedIn,[]);
+  netlifyIdentity.on('login', (user) => setLoggedIn(true))
+
+  useEffect(() => {
+    if (!loggedIn) {
+      netlifyIdentity.open()
+    } else {
+      netlifyIdentity.close()
+    }
+  }, [loggedIn])
 
   return (
-    <>
-    Logged: {loggedIn ? <>True</> : <>False</>}
-    </>
+    <div className={classes.root}>
+      <AppBar position="static" color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <Link to="/" color="inherit">
+              Cook Diary
+            </Link>
+          </Typography>
+          {loggedIn &&
+            <Link to="/add" color="inherit">
+              <AddIcon />
+            </Link>
+          }
+        </Toolbar>
+      </AppBar>
+    </div>
   )
 }
