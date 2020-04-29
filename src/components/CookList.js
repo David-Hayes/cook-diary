bc39971b2cebbe73cdeaac7e3fe036b9f7428f4e
David Hayes <davidhayes88@gmail.com>
2020-04-29 13:47:02 +0100

    Table in Cook list

--- src/components/CookList.js
+++ src/components/CookList.js
@@ -1,13 +1,23 @@
 import React, { useContext, useEffect } from 'react'
 import { GlobalContext } from '../context/State'
 
-import { Container, CircularProgress } from '@material-ui/core'
+import {
+  Container,
+  CircularProgress,
+  TableContainer,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
   loader: {
-    textAlign: 'center'
-  }
+    textAlign: 'center',
+  },
 }))
 
 export const CookList = () => {
@@ -18,10 +28,27 @@ export const CookList = () => {
 
   return (
     <Container>
-      {cooks.length > 0 &&
-      <div className={classes.loader}>
-        <CircularProgress />
-      </div>}
+      {cooks.length === 0 && (
+        <div className={classes.loader}>
+          <CircularProgress />
+        </div>
+      )}
+      {cooks.length > 0 && (
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Method</TableCell>
+                <TableCell>Time</TableCell>
+                <TableCell>Date</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
     </Container>
   )
 }
